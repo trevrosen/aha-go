8fed6d1305b2bf161a3c4b1e744e9a74f8772de4
Trevor Rosen <trevor@catapult-creative.com>
2013-09-05 16:28:14 -0500

    Fix README

--- gocode/src/hello/hello.go
+++ gocode/src/hello/hello.go
@@ -4,4 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, AHA!")
+
+  fmt.Println("--- Now we shall print an array of numbers")
+  someNums := []float64{1.2, 3.14, 2.71}
+  for _, x := range someNums {
+    fmt.Println("The numbers are:", x)
+  }
 }
